dddb13d8b2823680836e76af651a9e1f5958f966
dkwkdkwkdkwk <150777726+dkwkdkwkdkwk@users.noreply.github.com>
2026-01-21 20:04:32 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.13 ms, Memory: 9.04 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.py
@@ -0,0 +1,13 @@
+def solution(n):
+    
+    if n==1:
+        return 1
+    if n==2:
+        return 2
+        
+    a = 1
+    b = 2
+    for i in range(3,n+1):
+        a,b = b,(a+b)%1234567
+        
+    return b
